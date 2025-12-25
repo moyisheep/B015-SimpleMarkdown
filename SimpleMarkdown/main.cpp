091a344f17e5b96aee9dbb1d0952538ce83fea6d
MoYi-Sheep <MoYi-Sheep@LAPTOP>
2025-12-25 16:58:59 +0800

    V1.0 使用ai生成基本代码，开始慢慢优化

--- SimpleMarkdown/main.cpp
+++ SimpleMarkdown/main.cpp
@@ -1,5 +1,5 @@
 
-#include "wxContainer.h"
+#include "wxLitehtmlPanel.h"
 #include <wx/app.h>
 #include <wx/frame.h>
 
@@ -12,7 +12,7 @@ public:
 bool MyApp::OnInit()
 {
     wxFrame* frame = new wxFrame(nullptr, wxID_ANY, "LiteHtml with wxWidgets", wxDefaultPosition, wxSize(800, 600));
-    wxContainer* container = new wxContainer(frame);
+    wxLitehtmlPanel* container = new wxLitehtmlPanel(frame);
     frame->Show(true);
 
     std::string html = "<html><body><h1>Hello, World!</h1><p>This is a simple HTML page rendered using LiteHtml and wxWidgets.</p></body></html>";
--- SimpleMarkdown/wxContainer.cpp
+++
@@ -1,1 +0,0 @@
-#include "wxContainer.h"
